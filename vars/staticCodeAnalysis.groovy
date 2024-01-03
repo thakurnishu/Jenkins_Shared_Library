c813ec5407574ea633caca622626021102a44cca
thakurnishu <thakurnishant5102003@gmail.com>
2024-01-04 01:11:05 +0530

    Syntax Error

--- vars/staticCodeAnalysis.groovy
+++ vars/staticCodeAnalysis.groovy
@@ -1,6 +1,6 @@
 def call(Map params){
 
     withSonarQubeEnv(params.credentialsId){
-        sh '''$SCANNER_HOME/bin/sonar-scanner -Dsonar.projectKey=${params.projectKey}'''
+        sh "$SCANNER_HOME/bin/sonar-scanner -Dsonar.projectKey=${params.projectKey}"
     }
 }
