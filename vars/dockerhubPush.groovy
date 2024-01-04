9fea205086bb0f09ee93effd061c1613334ff95e
thakurnishu <thakurnishant5102003@gmail.com>
2024-01-04 13:25:58 +0530

    Added DockerHub Push file

--- vars/dockerhubPush.groovy
+++ vars/dockerhubPush.groovy
@@ -4,12 +4,12 @@ def call(String project, String userName, String imageTag, String credId){
         passwordVariable: 'PASS',
         usernameVariable: 'USER'
     )]) {
-        sh "docker login -u '$USER' -p '$PASS' "
+        sh """
+            docker login -u '$USER' -p '$PASS'
+            docker image push ${userName}/${project}:${imageTag}
+            docker image push ${userName}/${project}:latest
+            docker rmi ${userName}/${project}:${imageTag}
+            docker rmi ${userName}/${project}:latest
+        """
     }
-    sh """
-        docker image push ${userName}/${project}:${imageTag}
-        docker image push ${userName}/${project}:latest
-        docker rmi ${userName}/${project}:${imageTag}
-        docker rmi ${userName}/${project}:latest
-    """
 }
