8a65a70ed6b72793afe460bacd27db6f897ef5f2
thakurnishu <thakurnishant5102003@gmail.com>
2024-01-06 00:28:52 +0530

    corrected

--- vars/updateManifestK8S.groovy
+++ vars/updateManifestK8S.groovy
@@ -2,7 +2,7 @@ def call(Map params){
     sh """
         cd Manifest_Files
         cat deployment.yml
-        sed -i 's/${params.imageName}.*/${params.imageName}:${imageTag}'
+        sed -i 's/${params.imageName}.*/${params.imageName}:${imageTag}' deployment.yml
         cat deployment.yml
     """
 }
