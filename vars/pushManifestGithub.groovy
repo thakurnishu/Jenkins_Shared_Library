af25b9f39d54eb1631b61ce7d834790e746739d3
thakurnishu <thakurnishant5102003@gmail.com>
2024-01-05 23:40:10 +0530

    Added github push manifest file

---
+++ vars/pushManifestGithub.groovy
@@ -0,0 +1,11 @@
+def call(Map params){
+    sh """
+        git config --global user.name "${params.githubUserName}"
+        git config --global user.email "${params.githubEmail}"
+        git add Manifest_Files/deployment.yaml
+        git commit -m "Updated Deployment Manifest with ${params.imageTag} version"
+    """
+    withCredentials([usernamePassword(credentialsId: "${params.githubCredID}", passwordVariable: 'pass', usernameVariable: 'user')]){
+        sh "git push https://$user:$pass@${params.githubURL} main"
+    }
+}
