8c0769a106078336d7c3fa49e92853830d797c03
thakurnishu <thakurnishant5102003@gmail.com>
2024-01-05 18:48:22 +0530

    added install argocd file

---
+++ vars/installArgoCDToK8S.groovy
@@ -0,0 +1,14 @@
+def call(){
+    def isArgoCDInstalled = sh(script: 'kubectl get all -n argocd', returnStatus: true) == 0
+    if (!isArgoCDInstalled) {
+        sh """"
+            kubectl create namespace argocd
+            kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+            kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "LoadBalancer"}}'
+        """
+    } else {
+        sh """
+            echo 'ArgoCD is already installed. Skipping installation.'
+        """
+    }
+}
