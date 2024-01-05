269ad8769cdc06726a11b6c4ff809f9bf94d8651
thakurnishu <thakurnishant5102003@gmail.com>
2024-01-06 01:02:55 +0530

    added git branch

--- vars/pushManifestGithub.groovy
+++ vars/pushManifestGithub.groovy
@@ -4,6 +4,7 @@ def call(Map params){
         git config --global user.email "${params.githubEmail}"
         git add Manifest_Files/deployment.yml
         git commit -m "Updated Deployment Manifest with ${params.imageTag} version"
+        git branch -M main
     """
     withCredentials([usernamePassword(credentialsId: "${params.githubCredID}", passwordVariable: 'pass', usernameVariable: 'user')]){
         sh "git push https://$user:$pass@${params.githubURL} main"
