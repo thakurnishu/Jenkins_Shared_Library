5534293e0e226e15085e5f5140c6c27d29717cf7
thakurnishu <thakurnishant5102003@gmail.com>
2024-01-06 00:42:36 +0530

    corrected .yaml to .yml

--- vars/pushManifestGithub.groovy
+++ vars/pushManifestGithub.groovy
@@ -1,9 +1,8 @@
 def call(Map params){
     sh """
-        ls
         git config --global user.name "${params.githubUserName}"
         git config --global user.email "${params.githubEmail}"
-        git add Manifest_Files/deployment.yaml
+        git add Manifest_Files/deployment.yml
         git commit -m "Updated Deployment Manifest with ${params.imageTag} version"
     """
     withCredentials([usernamePassword(credentialsId: "${params.githubCredID}", passwordVariable: 'pass', usernameVariable: 'user')]){
