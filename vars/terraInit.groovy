86a567b78faa0139b1461d00543057d40e632c2e
thakurnishu <thakurnishant5102003@gmail.com>
2024-01-05 17:45:15 +0530

    added -reconfigure

--- vars/terraInit.groovy
+++ vars/terraInit.groovy
@@ -2,6 +2,7 @@ def call(StorageKey){
     sh """
         cd AKS_Module
         terraform init \
-        -backend-config="access_key=${StorageKey}"
+        -backend-config="access_key=${StorageKey}" \
+        -reconfigure -input=false -auto-approve
     """
 }
